e7786a09d24436279fa6099ab38ba01b23e8cb1b
katterine-Med <katerinm13@gmail.com>
2020-12-11 10:52:16 -0600

    avance pantalla

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,18 @@
 const cipher = {
-  metodos encode y decode
-
-
-
-
-
-
-
-
-
-
-
-};
-
+  
+  let string mensaje =document.getElementById("mensaje").value;
+  Console.log(mensaje)
+  }
+ 
+  cipher.encode(offset, string): offset;
+  let output= [];
+  for (let i=0; i <String.length; i++){
+    let asciiLetter= string.charCodeAt(i);
+    let stringResult= (((asciiLetter-65)+offset)%26)+65;
+    output[i]=String.fromCharCode(stringResult);
+    console.log(output);
+  
+  function descifrar(); {
+    
+  }
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import cipher from './cipher.js';
-
+document.getElementById('ok').addEventListener('click', DisplayView);
+function DisplayView(option) {
+    const ok=document.getElementById("ok");
+    const funcionamiento=document.getElementById("funcionamiento");
+    
+    switch (option){
+     case 1:
+      ok.style.display='block';
+      funcionamiento.style.display='none';
+      console.log (ok.style.display);
+      break;
+     case 2:
+      funcionamiento.style.display='block';
+      ok.style.display='none';
+      console.log(funcionamiento.style.display);
+      break;
+    }
+      
 console.log(cipher);
