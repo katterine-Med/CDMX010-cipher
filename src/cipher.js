101fbeb86142daedb35b0f472cdfaf9202f6ab9f
katterine-Med <katerinm13@gmail.com>
2020-12-16 22:36:17 -0600

    avance que funciona

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,28 @@
 const cipher = {
   
-  //let mensaje =document.getElementById("mensaje": string).value.toUpperCase();
- offset=document.getElementById('desplazamiento').value;
+ /*mensaje =document.getElementById("mensaje").value.toUpperCase();
+ offset =document.getElementById('desplazamiento').value;
  offset=parseInt(offset,(desplazamiento))
+ document.getElementById('cifrar')
   funtion cifrar (){
-  offset=document.getElementById('desplazamiento').value;
-  console.log (desplazamiento);
+    let output= [];
+    for (let i=0; i <string.length, i++){
+      let asciiLetter= string.charCodeAt(i);
+      let stringResult= (((asciiLetter-65)+offset)%26)+65);
+      output[i]=String.fromCharCode(stringResult);
+      console.log(stringResult);
+     output [i]= String.fromCharCode(stringResult;)
+    function descifrar(); {
+      console.log (offset);
+    }
+
+
  }
-  document.getElementById('cifrar')
-  Console.log(mensaje)*/
+ 
 
    //cipher.encode(offset, string): offset;
     let output= [];
-   for (let i=0; i <String.length; i++){
+   for (let i=0; i <string.length, i++){
      let asciiLetter= string.charCodeAt(i);
      let stringResult= (((asciiLetter-65)+offset)%26)+65);
      output[i]=String.fromCharCode(stringResult);
@@ -20,9 +30,9 @@ const cipher = {
     output [i]= String.fromCharCode(stringResult;)
    function descifrar(); {
      console.log (offset);
-  }
+   }
  
-  
+  */
     
   }
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 import cipher from './cipher.js';
 
-/*document.getElementById('ok').addEventListener('click', DisplayView);
-function DisplayView(option) {
-    const ok=document.getElementById("ok", string)HTMLElement;
-    const funcionamiento=document.getElementById("funcionamiento");
-    
-    switch (option){
-     case 1:
-      ok.style.display='block';
-      funcionamiento.style.display='none';
-      console.log (ok.style.display);
-      break;
-     case 2:
-      funcionamiento.style.display='block';
-      ok.style.display='none';
-      console.log(funcionamiento.style.display);
-      break;
-    }
-} */
+document.getElementById('okF').addEventListener("click",inicio,false);
 
-const input= document.getElementById('mensaje')
-      input.addEventListener('input',function(evt){
-          input.value= evt.target.value.toUpperCase()
-         })
+//document.getElementById('ok').addEventListener('click', DisplayView);//
+function inicio() {
+    let siguiente = document.getElementById('pantalla2');
+    document.getElementById("pantalla1").style.display="none";
+    siguiente.style.display='block';
+   
+} 
+function final() {
+    document.getElementById("pantalla2").style.display="none";
+    document.getElementById("pantalla1").style.display="block";
+   
+} 
+ const input = document.getElementById('mensaje')
+ input.addEventListener('input', function(evt){
+ 
+     input.value = evt.target.value.toUpperCase()})
+ 
  /*function cifrar (){
         let offset  =document.getElementById('desplazamiento').value;
         alert(desplazamiento);
        }
         document.getElementById('cifrar').addEventListener('click', cifrar);*/
-      
-console.log(cipher);
+    
+//console.log(cipher);
