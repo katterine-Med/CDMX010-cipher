76b734aa2d496d45c2e1fff648ee95ebffd24c46
katterine-Med <katerinm13@gmail.com>
2020-12-18 10:00:04 -0600

    avance cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,15 @@
 const cipher = {
+
+encode:function(offset,string){
+  let output= [];
+  for (let i=0; i <string.length; i++){
+    let asciiLetter= string.charCodeAt(i)
+    let stringResult= (((asciiLetter-65)+offset)%26+65); //seguramente algo de aqui esta mal
+    output [i]= String.fromCharCode(stringResult)
+  }
+  return output //retornar un string
+}
+
   
  /*mensaje =document.getElementById("mensaje").value.toUpperCase();
  offset =document.getElementById('desplazamiento').value;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import cipher from './cipher.js';
+//const  document.getElementById ('mensaje');
 
-document.getElementById('okF').addEventListener("click",inicio,false);
+
+
+document.getElementById('okF').addEventListener("click", inicio, false);
 
 //document.getElementById('ok').addEventListener('click', DisplayView);//
 function inicio() {
     let siguiente = document.getElementById('pantalla2');
-    document.getElementById("pantalla1").style.display="none";
-    siguiente.style.display='block';
-   
-} 
+    document.getElementById("pantalla1").style.display = "none";
+    siguiente.style.display = 'block';
+
+}
 function final() {
-    document.getElementById("pantalla2").style.display="none";
-    document.getElementById("pantalla1").style.display="block";
-   
-} 
- const input = document.getElementById('mensaje')
- input.addEventListener('input', function(evt){
- 
-     input.value = evt.target.value.toUpperCase()})
- 
+    document.getElementById("pantalla2").style.display = "none";
+    document.getElementById("pantalla1").style.display = "block";
+
+}
+const input = document.getElementById('mensaje')
+input.addEventListener('input', function (evt) {
+
+    input.value = evt.target.value.toUpperCase()
+})
+
+console.log(cipher.encode(3,"CASA")) 
+document.getElementById ('mensaje2').textContent = value;
+//en un futuro tienes que borrrar esto pero de momento nos sirve para ejecutar la funcion encode
+
  /*function cifrar (){
-        let offset  =document.getElementById('desplazamiento').value;
-        alert(desplazamiento);
-       }
-        document.getElementById('cifrar').addEventListener('click', cifrar);*/
-    
+       let offset  =document.getElementById('desplazamiento').value;
+       alert(desplazamiento);
+      }
+       document.getElementById('cifrar').addEventListener('click', cifrar);*/
+
 //console.log(cipher);
