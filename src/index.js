d9475d8cb54c3a43733ded1fb59c8cc35b70abe8
katterine-Med <katerinm13@gmail.com>
2021-01-05 11:54:39 -0600

    Is the last version

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ function inicio() {
     siguiente.style.display = 'block';
 
 }
-function final() {
+/*function final() {
     document.getElementById("pantalla2").style.display = "none";
     document.getElementById("pantalla1").style.display = "block";
 
-}
+}*/
 //funcion para hacer mayusculas al ingresar texto usuario
 const input = document.getElementById('mensaje')
 input.addEventListener('input', function (evt) {
@@ -29,7 +29,7 @@ var desplazamiento = document.getElementById("desplazamiento");
 document.getElementById('cifrar').addEventListener("click", funcionCifrar, false);
 document.getElementById('descifrar').addEventListener("click", funcionDescifrar,false);
 
-// //funcion para cifrar
+ //funcion para cifrar
 
 function funcionCifrar() {
     var des = Number.parseInt(desplazamiento.value);
@@ -44,13 +44,11 @@ function funcionDescifrar() {
     var mensaje_descifrado = cipher.decode(des, output.value);
 
     document.getElementById('mensaje3').textContent = mensaje_descifrado;
-    console.log(mensaje_descifrado);
 }
-console.log("hola");
+
 
 //metodo encode y decode con parametro desplazamiento y cadena
 // una funcion se declara como tal, al llamar una funcion se ejecuta una accion
 /*para invocar un metodo se declara al objeto o clase, un metodo es la capacidad de un objeto. 
 un conjunto de funcionalidades que dependen del objeto*/
 //objeto es una coleccion de propiedades ejem: nombre y un valor
-console.log(cipher);
